6a2c0b7bbe1a496f893e3d6d653f5918aea270eb
Tanner Chen <402821051@qq.com>
2025-07-27 19:11:44 +0800

    x

--- Mir2Assistant.Common/Models/GameAccountModel.cs
+++ Mir2Assistant.Common/Models/GameAccountModel.cs
@@ -19,7 +19,6 @@ namespace Mir2Assistant.Common.Models
         public string Password { get; set; } = string.Empty;
         public int Gender { get; set; } = 0; // 0 女 1 男
         public string CharacterName { get; set; } = string.Empty;
-        public string encodeArg { get; set; } = string.Empty;
 
         public bool IsMainControl { get; set; }
         public RoleType role { get; set; }
