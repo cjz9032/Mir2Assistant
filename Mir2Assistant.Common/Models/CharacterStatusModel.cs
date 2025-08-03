40a4b2ac9b2d188dc4070c5c78d94e97841471f7
Tanner Chen <402821051@qq.com>
2025-08-03 09:00:21 +0800

    def

--- Mir2Assistant.Common/Models/CharacterStatusModel.cs
+++ Mir2Assistant.Common/Models/CharacterStatusModel.cs
@@ -2,18 +2,18 @@
 
 public class CharacterStatusModel
 {
-    public string Name { get; set; }
-    public string MapName { get; set; }
-    public string MapId { get; set; }
-    public int X { get; set; }
-    public int Y { get; set; }
-    public int CurrentHP { get; set; }
-    public int MaxHP { get; set; }
-    public int CurrentMP { get; set; }
-    public int MaxMP { get; set; }
-    public byte Level { get; set; }
-    public byte groupMemCount { get; set; }
-    public bool allowGroup { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string MapName { get; set; } = string.Empty;
+    public string MapId { get; set; } = string.Empty;
+    public int X { get; set; } = 0;
+    public int Y { get; set; } = 0;
+    public int CurrentHP { get; set; } = 0;
+    public int MaxHP { get; set; } = 0;
+    public int CurrentMP { get; set; } = 0;
+    public int MaxMP { get; set; } = 0;
+    public byte Level { get; set; } = 0;
+    public byte groupMemCount { get; set; } = 0;
+    public bool allowGroup { get; set; } = false;
 
     // 装备9格
     public List<ItemModel> useItems { get; set; } = new List<ItemModel>();
