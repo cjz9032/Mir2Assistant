53f2334d0aed15ddb07700ceba184e3d94838c24
Tanner Chen <402821051@qq.com>
2025-09-30 22:51:59 +0800

    tempt for full

--- Mir2Assistant.Common/Functions/GoRunFunction.cs
+++ Mir2Assistant.Common/Functions/GoRunFunction.cs
@@ -1030,6 +1030,7 @@ public static class GoRunFunction
                         // 寻找陀螺
                         var mytop = instanceValue.Monsters.Values.Where(o => o.stdAliveMon
                         && GameConstants.TempMonsterLevels.GetValueOrDefault(o.Name, 99) <= (CharacterStatus.Level + 2)
+                        && (o.CurrentHP == 0 || o.CurrentHP == o.MaxHP)
                         && GameConstants.allowTemp.Contains(o.Name)
                         && Math.Max(Math.Abs(o.X - CharacterStatus.X), Math.Abs(o.Y - CharacterStatus.Y)) < 10)
                         .OrderBy(o => Math.Max(Math.Abs(o.X - CharacterStatus.X), Math.Abs(o.Y - CharacterStatus.Y)))
