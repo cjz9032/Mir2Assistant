aa7835677234475ff1b4b22036ca05708e1be55c
Tanner Chen <402821051@qq.com>
2025-10-01 17:00:17 +0800

    x

--- Mir2Assistant/MainForm.cs
+++ Mir2Assistant/MainForm.cs
@@ -1245,7 +1245,7 @@ namespace Mir2Assistant
                                         && instanceValue.CharacterStatus.CurrentMP < instanceValue.CharacterStatus.MaxMP * 0.2);
 
 
-                                        var isOtherLowHp = instanceValue.AccountInfo.role != RoleType.taoist && (instanceValue.CharacterStatus.CurrentHP < instanceValue.CharacterStatus.MaxHP * 0.1 || instanceValue.CharacterStatus.CurrentHP < 20);
+                                        var isOtherLowHp = instanceValue.AccountInfo.role != RoleType.taoist && (instanceValue.CharacterStatus.CurrentHP < instanceValue.CharacterStatus.MaxHP * 0.1 || (instanceValue.CharacterStatus.MaxHP > 40 && instanceValue.CharacterStatus.CurrentHP < 20));
                                         // 主号没药
 
 
