6f7506aebbb62de8103aeca360b72590eadcd253
Tanner Chen <402821051@qq.com>
2025-10-11 01:54:31 +0800

    t

--- Mir2Assistant/MainForm.cs
+++ Mir2Assistant/MainForm.cs
@@ -1644,7 +1644,7 @@ namespace Mir2Assistant
                             // 用用再说
                             if (GameState.gamePath == "Client.exe")
                             {
-                                var sp = (instance.CharacterStatus.Level < 11 || instance.AccountInfo!.role == RoleType.mage) ? 130 : 95;
+                                var sp = (instance.CharacterStatus.Level < 11 || instance.AccountInfo!.role == RoleType.mage) ? 180 : 95;
                                 CharacterStatusFunction.AdjustMoveSpeed(instance, sp);
                             }
                             else
@@ -1655,7 +1655,7 @@ namespace Mir2Assistant
                             {
                                 if (GameState.gamePath == "Client.exe")
                                 {
-                                    CharacterStatusFunction.AdjustAttackSpeed(instance, 1000);
+                                    CharacterStatusFunction.AdjustAttackSpeed(instance, 1200);
                                 }
                                 else
                                 {
