a8ac6108751f9e21200ae284dca72e105c44f964
Tanner Chen <402821051@qq.com>
2025-10-17 21:45:37 +0800

    t

--- Mir2Assistant/MainForm.cs
+++ Mir2Assistant/MainForm.cs
@@ -1248,9 +1248,11 @@ namespace Mir2Assistant
 
 
                                 instanceValue.isHomePreparing = false;
-                                var slaveFromMap = "E605";
-                                var slaveToMap = "E702";
-                                var slaveEnabled = false; // CharacterStatus.Level >= 30 && true;
+                                // var slaveFromMap = "E605";
+                                // var slaveToMap = "E702";
+                                var slaveFromMap = "M100";
+                                var slaveToMap = "D2001";
+                                var slaveEnabled = true; // CharacterStatus.Level >= 30 && true;
                                 if (slaveEnabled)
                                 {
                                     hangMapId = hangMapBBReady ? slaveToMap : slaveFromMap;
